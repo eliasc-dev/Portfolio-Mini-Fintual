a6297a6246b3f3eb5eb253ca2b2c85da1b2b83af
eliasc-dev <e.carrenoarriagada@gmail.com>
2026-04-26 23:30:42 -0400

    Implementación clase "Usuario".

--- models.py
+++ models.py
@@ -1,3 +1,8 @@
+class Usuario:
+   def __init__(self, nombre, saldo_disponible):
+      self.nombre = nombre
+      self.saldo_disponible = saldo_disponible
+
 class Fondo:
    def __init__(self, nombre, saldo):
       self.nombre = nombre
@@ -6,3 +11,4 @@ class Fondo:
    def simular(self, tasa, meses):
       resultado = self.saldo*(1+tasa)**meses
       return resultado
+
